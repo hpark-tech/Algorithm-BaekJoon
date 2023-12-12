86f7951e220ef31f10ec5dc1681477f407aec17c
hpark-tech <119441604+hpark-tech@users.noreply.github.com>
2023-12-13 02:08:02 +0900

    [Bronze III] Title: 오븐 시계, Time: 236 ms, Memory: 18456 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.java
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -5,14 +5,13 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
 		String str = sc.nextLine();
 		String[] arr = str.split(" ");
 		int hour = Integer.parseInt(arr[0]);
 		int min = Integer.parseInt(arr[1]);
 		int cookMin = sc.nextInt();
 		int deadHour = hour + (cookMin / 60);
-		int deadMin = cookMin % 60 + min;
+		int deadMin = (cookMin % 60) + min;
 		if (deadMin > 60) {
 			deadMin -= 60;
 			deadHour += 1;
@@ -22,7 +21,7 @@ public class Main {
 			deadHour += 1;
 		}
 		if (deadHour > 23) {
-			deadHour = deadHour % 24;
+			deadHour %= 24;
 		}
 		System.out.println(deadHour + " " + deadMin);
 	}
