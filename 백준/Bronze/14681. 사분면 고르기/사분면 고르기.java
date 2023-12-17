d29cfc3e0322b1fc589c8c3e2d2a339d5330059c
hpark-tech <119441604+hpark-tech@users.noreply.github.com>
2023-12-17 17:38:18 +0900

    [Bronze V] Title: 사분면 고르기, Time: 208 ms, Memory: 17716 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -10,14 +10,11 @@ public class Main {
 
 		if (x > 0 && y > 0) {
 			System.out.println(1);
-		}
-		if (x < 0 && y > 0) {
+		} else if (x < 0 && y > 0) {
 			System.out.println(2);
-		}
-		if (x < 0 && y < 0) {
+		} else if (x < 0 && y < 0) {
 			System.out.println(3);
-		}
-		if (x > 0 && y < 0) {
+		} else {
 			System.out.println(4);
 		}
 		sc.close();
