8943fe636795571a079f08b437667aadecb6a19e
hpark-tech <119441604+hpark-tech@users.noreply.github.com>
2023-12-17 14:46:40 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 240 ms, Memory: 18316 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,31 @@
+
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		char arr[] = new char[123];
+		for (char i = 'a'; i <= 'z'; i++) {
+			for (int j = 0; j < s.length(); j++) {
+				if (i != 'z' && i == s.charAt(j)) {
+					System.out.print(j + " ");
+					arr[i] = 1;
+					break;
+				}
+				if (i == 'z' && i == s.charAt(j)) {
+					System.out.print(j);
+					arr[i] = 1;
+					break;
+				}
+			}
+			if (arr[i] == 0 && i != 'z') {
+				System.out.print(-1 + " ");
+			}
+			if (arr[i] == 0 && i == 'z') {
+				System.out.print(-1);
+			}
+		}
+	}
+}
