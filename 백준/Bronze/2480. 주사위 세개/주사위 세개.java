106367c27c9425e820aaf946de334bb459352f05
hpark-tech <119441604+hpark-tech@users.noreply.github.com>
2023-12-17 18:31:31 +0900

    [Bronze IV] Title: 주사위 세개, Time: 208 ms, Memory: 17608 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,32 @@
+
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int max = a;
+		int min = a;
+		if (b > max) {
+			max = b;
+		}
+		if (c > max) {
+			max = c;
+		}
+		if (a == b && b == c) {
+			System.out.println(10000 + a * 1000);
+		} else if (a == b) {
+			System.out.println(1000 + a * 100);
+		} else if (c == b) {
+			System.out.println(1000 + c * 100);
+		} else if (a == c) {
+			System.out.println(1000 + a * 100);
+		} else {
+			System.out.println(max*100);
+		}
+		sc.close();
+	}
+}
