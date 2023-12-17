3b25fe4222f7d2531b15406b7ba916081ea2c287
hpark-tech <119441604+hpark-tech@users.noreply.github.com>
2023-12-17 19:00:42 +0900

    [Bronze IV] Title: 영수증, Time: 224 ms, Memory: 17848 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int allcost = sc.nextInt();
+		int allkind = sc.nextInt();
+		int cal = 0;
+		String a = "long ";
+		String k ="";
+		for (int i = 0; i < allkind; i++) {
+			int cost = sc.nextInt();
+			int num = sc.nextInt();
+			cal += cost*num;
+		}
+		if(cal==allcost) {System.out.println("Yes");}
+		else {System.out.println("No");}
+		sc.close();
+	}
+}
