9d9b8ce941f507651f70aa9151a2ccd92b3f55c7
hpark-tech <119441604+hpark-tech@users.noreply.github.com>
2023-12-19 12:24:00 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 588 ms, Memory: 23296 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,35 @@
+
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		String s = "s";
+		for (int i = 0; i < num; i++) {
+			for (int j = num-1; j > i; j--) {
+				System.out.print(" ");
+			}
+			for (int k = -1; k < i; k++) {
+				System.out.print("*");
+			}
+			for (int h = 0; h < i; h++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for (int i = 0; i < num-1; i++) {
+			for (int k = -1; k < i; k++) {
+				System.out.print(" ");
+			}
+			for (int j = num- 2; j > i; j--) {
+				System.out.print("*");
+			}
+			for (int h = num- 1; h > i; h--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
