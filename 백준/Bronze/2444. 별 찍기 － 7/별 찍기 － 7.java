998f65aa6bdb5a6eebe7a7684c0667b3df61e8ec
hpark-tech <119441604+hpark-tech@users.noreply.github.com>
2023-12-20 12:19:49 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 528 ms, Memory: 20600 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -1,12 +1,9 @@
-
-
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
-		String s = "s";
 		for (int i = 0; i < num; i++) {
 			for (int j = num-1; j > i; j--) {
 				System.out.print(" ");
