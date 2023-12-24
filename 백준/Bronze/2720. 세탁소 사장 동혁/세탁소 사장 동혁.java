abac70e437a0902b9eade055600fbaebeed3852c
hpark-tech <119441604+hpark-tech@users.noreply.github.com>
2023-12-25 03:47:42 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 348 ms, Memory: 19020 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
@@ -8,16 +8,10 @@ public class Main {
 		int k = sc.nextInt();
 		for (int i = 0; i < k; i++) {
 			int c = sc.nextInt();
-			if (c >= 25) {
-				System.out.println(
-						c / 25 + " " + (c % 25) / 10 + " " + ((c % 25) % 10) / 5 + " " + (((c % 25) % 10) % 5) / 1);
-			} else if (c < 25 && c >= 10) {
-				System.out.println("0" + " "+c / 10 + " " + (c % 10) / 5 + " " + ((c % 10) % 5) / 1);
-			} else if (c < 10 && c >= 5) {
-				System.out.println("0" + " "+ 0 + " "+c / 5 + " " + (c % 5) / 1);
-			} else if (c < 5 && c >= 1) {
-				System.out.println("0" + " "+ 0 + " "+ 0+ " " +c / 1);
-			}
+			System.out.print(c / 25 + " ");
+			System.out.print(c % 25 / 10 + " ");
+			System.out.print(c % 25 % 10 / 5 + " ");
+			System.out.println(c % 25 % 10 % 5 / 1);
 		}
 	}
 }
