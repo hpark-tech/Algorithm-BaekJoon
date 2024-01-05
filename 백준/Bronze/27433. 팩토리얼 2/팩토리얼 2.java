d9e90fe1500935a28a832450fec2baacf1be6932
hpark-tech <119441604+hpark-tech@users.noreply.github.com>
2024-01-05 20:47:12 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 144 ms, Memory: 15932 KB -BaekjoonHub

--- 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -1,3 +1,5 @@
+
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -21,3 +23,17 @@ public class Main {
 		bw.flush();
 	}
 }
+
+//import java.util.Scanner;
+//
+//public class Main {
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		int n = sc.nextInt();
+//		long result = 1;
+//		for (int i = 1; i <= n; i++) {
+//			result *= i;
+//		}
+//		System.out.println(result);
+//	}
+//}
