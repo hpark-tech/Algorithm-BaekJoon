e8ffdf5ffea94042b730f171210bd13cb813f867
hpark-tech <119441604+hpark-tech@users.noreply.github.com>
2024-01-05 20:46:27 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 148 ms, Memory: 14608 KB -BaekjoonHub

--- 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -1,15 +1,23 @@
-
-
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		long result = 1;
-		for (int i = 1; i <= n; i++) {
-			result *= i;
+	public static long factorial(int a) {
+		if (a == 0) {
+			return 1;
+		} else {
+			return a * factorial(a - 1);
 		}
-		System.out.println(result);
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine());
+		bw.write(factorial(n) + "\n");
+		bw.flush();
 	}
 }
