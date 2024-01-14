b6bb1cb43d2194fa839ef5f550505102d2733a95
hpark-tech <119441604+hpark-tech@users.noreply.github.com>
2024-01-14 23:32:12 +0900

    [Bronze V] Title: 팩토리얼, Time: 140 ms, Memory: 14584 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,26 @@
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	static int factorial(int n) {
+		if (n == 0) {
+			return 1;
+		} else {
+			return factorial(n - 1) * n;
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine());
+		bw.write(factorial(n) + "\n");
+		bw.flush();
+	}
+}
