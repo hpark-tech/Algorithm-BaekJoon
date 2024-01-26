ccedb9c7c53c60dab129c052233a1ba443aed8ba
hpark-tech <119441604+hpark-tech@users.noreply.github.com>
2024-01-26 21:49:21 +0900

    [Bronze II] Title: 분해합, Time: 156 ms, Memory: 15020 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -0,0 +1,37 @@
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine());
+		List<Integer> list = new ArrayList<>();
+		for (int i = 0; i < n; i++) {
+			int mRemainderSum = 0;
+			int m = i;
+			while (m > 0) {
+				mRemainderSum += (m % 10);
+				m /= 10;
+			}
+			if (n == (i + mRemainderSum)) {
+				list.add(i);
+			}
+		}
+		if (!list.isEmpty()) {
+			Collections.sort(list);
+			bw.write(list.get(0) + "\n");
+		} else {
+			bw.write("0" + "\n");
+		}
+		bw.flush();
+	}
+}
