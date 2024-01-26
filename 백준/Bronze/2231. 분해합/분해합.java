e952135a0f3cdac49cde835771d876de06e14c0e
hpark-tech <119441604+hpark-tech@users.noreply.github.com>
2024-01-26 22:00:39 +0900

    [Bronze II] Title: 분해합, Time: 156 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -1,12 +1,9 @@
-
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Main {
@@ -24,10 +21,10 @@ public class Main {
 			}
 			if (n == (i + mRemainderSum)) {
 				list.add(i);
+				break;
 			}
 		}
 		if (!list.isEmpty()) {
-			Collections.sort(list);
 			bw.write(list.get(0) + "\n");
 		} else {
 			bw.write("0" + "\n");
