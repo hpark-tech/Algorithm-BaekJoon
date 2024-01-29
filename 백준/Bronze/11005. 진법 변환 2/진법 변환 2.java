e0c458a3a682e63200c32356f5c76666dfd6f4b5
hpark-tech <119441604+hpark-tech@users.noreply.github.com>
2024-01-30 04:04:19 +0900

    [Bronze I] Title: 진법 변환 2, Time: 136 ms, Memory: 14412 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -0,0 +1,42 @@
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer token;
+		token = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(token.nextToken());
+		int b = Integer.parseInt(token.nextToken());
+		ArrayList<Integer> remainderList = new ArrayList<>();
+		int quotient = n;
+		while (quotient >= b) {
+			remainderList.add(quotient % b);
+			quotient /= b;
+		}
+		if (quotient >= 10) {
+			bw.write((char) (quotient + 55) + "");
+		} else {
+			bw.write(quotient + "");
+		}
+		Collections.reverse(remainderList);
+		for (int k : remainderList) {
+			if (k >= 10) {
+				bw.write((char) (k + 55) + "");
+			} else {
+				bw.write(k + "");
+			}
+		}
+		bw.write("\n");
+		bw.flush();
+	}
+}
