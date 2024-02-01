f04456d4c841881868f0d928106eb49b8124115a
hpark-tech <119441604+hpark-tech@users.noreply.github.com>
2024-02-01 23:58:15 +0900

    Update 카드2.java

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,27 +1,26 @@
+package baekjoon;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Iterator;
 import java.util.LinkedList;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		LinkedList<Integer> queue = new LinkedList<>();
-		
-		int n = Integer.parseInt(br.readLine());	
-		
-		for(int i=1; i<=n; i++)
+		int n = Integer.parseInt(br.readLine());
+		for (int i = 1; i <= n; i++) {
 			queue.offer(i);
-		
-		while(queue.size()>1) {
-			queue.poll();
-			queue.offer(queue.poll());
 		}
-		
-		System.out.println(queue.poll());
+		while (queue.size() > 1) {
+			queue.pollFirst();
+			queue.offerLast(queue.pollFirst());
+		}
+		bw.write(queue.poll() + "\n");
+		bw.flush();
 	}
 }
