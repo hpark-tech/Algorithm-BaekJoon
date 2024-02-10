36ca25b866e1820037f2e09e4b9fac3be3323645
hpark-tech <119441604+hpark-tech@users.noreply.github.com>
2024-02-10 21:07:38 +0900

    [Silver III] Title: N과 M (1), Time: 2252 ms, Memory: 52164 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,41 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static int arrM[];
+	public static boolean visit[];
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer token = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(token.nextToken());
+		int m = Integer.parseInt(token.nextToken());
+		arrM = new int[m];
+		visit = new boolean[n];
+		int depth = 0;
+		reculsion(n, m, depth);
+	}
+
+	public static void reculsion(int n, int m, int depth) {
+		if (depth == m) {
+			for (int k : arrM) {
+				System.out.print(k + " ");
+			}
+			System.out.println();
+			return;
+		}
+		for (int i = 0; i < n; i++) {
+			if (visit[i] == false) {
+				arrM[depth] = i + 1;
+				visit[i] = true;
+				reculsion(n, m, depth + 1);
+				visit[i] = false;
+			}
+		}
+	}
+
+}
