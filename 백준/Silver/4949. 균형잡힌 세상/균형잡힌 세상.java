3d28049061cc41fac0d91d49dece4ebcbe488603
hpark-tech <119441604+hpark-tech@users.noreply.github.com>
2024-02-21 04:14:49 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 352 ms, Memory: 20216 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -1,44 +1,56 @@
-import java.util.Scanner;
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Stack;
 
-
 public class Main {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String n;
-
-        while (true) {
-            n = scan.nextLine();
-            if (n.equals(".")) {
-                break;
-            } else
-                System.out.println(result(n));
-        }
-    }
-
-    private static String result(String n) {
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < n.length(); i++) {
-            char k = n.charAt(i);
-            if (k == '(' || k == '[')
-                stack.push(k);
-
-            else if (k == ')') {
-                if (stack.isEmpty() || stack.peek() != '(')
-                    return "no";
-                else
-                    stack.pop();
-            } else if (k == ']') {
-                if (stack.isEmpty() || stack.peek() != '[')
-                    return "no";
-                else
-                    stack.pop();
-            }
-        }
-
-        if (stack.isEmpty())
-            return "yes";
-        else
-            return "no";
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		Stack<Character> stack;
+		while (true) {
+			stack = new Stack<>();
+			String n = br.readLine();
+			if (n == null) {
+				break;
+			}
+			for (int i = 0; i < n.length(); i++) {
+				char k = n.charAt(i);
+				if (k == '(' || k == '[') {
+					stack.push(k);
+				} else if (!stack.empty() && k == ')') {
+					if (stack.peek() == '(') {
+						stack.pop();
+					} else {
+						stack.push(k);
+						break;
+					}
+				} else if (!stack.empty() && k == ']') {
+					if (stack.peek() == '[') {
+						stack.pop();
+					} else {
+						stack.push(k);
+						break;
+					}
+				} else if (stack.empty() && (k == ']' || k == ')')) {
+					stack.push(k);
+					break;
+				}
+				if (k == '.') {
+					break;
+				}
+			}
+			if (n.equals(".")) {
+			} else if (stack.empty()) {
+				bw.write("yes" + "\n");
+			} else {
+				bw.write("no" + "\n");
+			}
+			bw.flush();
+		}
+	}
 }
