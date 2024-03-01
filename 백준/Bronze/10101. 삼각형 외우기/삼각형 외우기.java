e9c02ef6edb95cd2e4a5041b546b41dc341e145a
hpark-tech <119441604+hpark-tech@users.noreply.github.com>
2024-03-02 00:00:14 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 120 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.java
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.java
@@ -14,14 +14,12 @@ public class Main {
 
 		if (sum != 180) {
 			System.out.println("Error");
-		} else {
-			if (a == b && b == c) {
-				System.out.println("Equilateral");
-			} else if (a == b || b == c || a == c) {
-				System.out.println("Isosceles");
-			} else {
-				System.out.println("Scalene");
-			}
+		} else if (a == 60 && b == 60 && c == 60) {
+			System.out.println("Equilateral");
+		} else if ((a == b || b == c || a == c) && sum == 180) {
+			System.out.println("Isosceles");
+		} else if ((a != b && b != c && a != c) && sum == 180) {
+			System.out.println("Scalene");
 		}
 	}
 }
