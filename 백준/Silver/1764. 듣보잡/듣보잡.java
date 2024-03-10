0081f28dbda929542f5b077e9dbbaf2e860ff4de
hpark-tech <119441604+hpark-tech@users.noreply.github.com>
2024-03-11 00:06:45 +0900

    [Silver IV] Title: 듣보잡, Time: 324 ms, Memory: 29048 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -2,47 +2,40 @@
 
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N;
-	static int M;
 	public static void main(String[] args) throws IOException {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
-
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		HashSet<String> set = new HashSet<>();
-
-		ArrayList<String> strList = new ArrayList<String>();
-
-		for (int i = 0; i < N; i++) {
-			set.add(br.readLine());
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer token = new StringTokenizer(br.readLine());
+		HashSet<String> hs = new HashSet<>();
+		ArrayList<String> list = new ArrayList<>();
+		int n = Integer.parseInt(token.nextToken());
+		int m = Integer.parseInt(token.nextToken());
+		int count = 0;
+		for (int i = 0; i < n; i++) {
+			hs.add(br.readLine());
 		}
-
-		for (int i = 0; i < M; i++) {
-			String str = br.readLine();
-			if (set.contains(str)) {
-				strList.add(str);
+		for (int i = 0; i < m; i++) {
+			String temp = br.readLine();
+			if (hs.add(temp) == false) {
+				list.add(temp);
+				count++;
 			}
-
 		}
-
-		Collections.sort(strList);
-
-		System.out.println(strList.size());
-
-		for (String s : strList) {
-			System.out.println(s);
+		Collections.sort(list);
+		bw.write(count + "\n");
+		for (String k : list) {
+			bw.write(k + "\n");
 		}
+		bw.flush();
 	}
-
 }
