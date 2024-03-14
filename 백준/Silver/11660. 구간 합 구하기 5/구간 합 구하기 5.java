37a11d614739ad285fb847f1730e9e4e50c20266
hpark-tech <119441604+hpark-tech@users.noreply.github.com>
2024-03-15 00:50:44 +0900

    Update 구간 합 구하기 5.java

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -7,42 +8,39 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer token = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(token.nextToken());
-        int m = Integer.parseInt(token.nextToken());
-        int[][] arr = new int[n + 1][n + 1];
-
-        for (int i = 1; i <= n; i++) {
-            token = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= n; j++) {
-                arr[i][j] = Integer.parseInt(token.nextToken());
-            }
-        }
-
-        int[][] prefixSum = new int[n + 1][n + 1];
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                prefixSum[i][j] = arr[i][j] + prefixSum[i - 1][j] + prefixSum[i][j - 1] - prefixSum[i - 1][j - 1];
-            }
-        }
-
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < m; i++) {
-            token = new StringTokenizer(br.readLine());
-            int x1 = Integer.parseInt(token.nextToken());
-            int y1 = Integer.parseInt(token.nextToken());
-            int x2 = Integer.parseInt(token.nextToken());
-            int y2 = Integer.parseInt(token.nextToken());
-            int queryResult = prefixSum[x2][y2] - prefixSum[x1 - 1][y2] - prefixSum[x2][y1 - 1] + prefixSum[x1 - 1][y1 - 1];
-            result.append(queryResult).append("\n");
-        }
-
-        bw.write(result.toString());
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer token = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(token.nextToken());
+		int m = Integer.parseInt(token.nextToken());
+		int arr[][] = new int[n + 1][n + 1];
+		for (int i = 1; i <= n; i++) {
+			token = new StringTokenizer(br.readLine());
+			for (int j = 1; j <= n; j++) {
+				if (j > 1) {
+					arr[i][j] = arr[i][j - 1] + Integer.parseInt(token.nextToken());
+				} else {
+					arr[i][j] = Integer.parseInt(token.nextToken());
+				}
+			}
+		}
+		for (int i = 0; i < m; i++) {
+			int result = 0;
+			token = new StringTokenizer(br.readLine());
+			int x1 = Integer.parseInt(token.nextToken());
+			int x2 = Integer.parseInt(token.nextToken());
+			int y1 = Integer.parseInt(token.nextToken());
+			int y2 = Integer.parseInt(token.nextToken());
+			for (int j = x1; j <= y1; j++) {
+				if (arr[j][y2] != arr[j][x2 - 1]) {
+					result += arr[j][y2] - arr[j][x2 - 1];
+				} else {
+					result = arr[j][y2];
+				}
+			}
+			bw.write(result + "\n");
+		}
+		bw.flush();
+	}
 }
