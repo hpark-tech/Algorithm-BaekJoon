d2616df06edfb4b7ceef60ff2a18662049c7fce9
hpark-tech <119441604+hpark-tech@users.noreply.github.com>
2024-03-16 00:20:14 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 244 ms, Memory: 24588 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -1,23 +1,21 @@
-import java.util.Scanner;
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
-import java.io.IOException;
- 
 
- 
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 public class Main {
 	static char[][] arr;
  
 	public static void main(String[] args) throws IOException {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		Scanner in = new Scanner(System.in);
-		int N = in.nextInt();
- 
-		arr = new char[N][N];
-        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		arr = new char[N][N];      
 		star(0, 0, N, false);
  
-
 		for (int i = 0; i < N; i++) {
 			bw.write(arr[i]);	
 			bw.write("\n");
@@ -28,7 +26,6 @@ public class Main {
  
 	static void star(int x, int y, int N, boolean blank) {
  
-
 		if (blank) {
 			for (int i = x; i < x + N; i++) {
 				for (int j = y; j < y + N; j++) {
@@ -38,19 +35,17 @@ public class Main {
 			return;
 		}
  
-
 		if (N == 1) {
 			arr[x][y] = '*';
 			return;
 		}
- 
- 
+
 		int size = N / 3;
 		int count = 0;
 		for (int i = x; i < x + N; i += size) {
 			for (int j = y; j < y + N; j += size) {
 				count++;
-				if (count == 5) { 
+				if (count == 5) {
 					star(i, j, size, true);
 				} else {
 					star(i, j, size, false);
