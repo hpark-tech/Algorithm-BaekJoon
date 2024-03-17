70a4a6b3e4e4715aaf3f8f131383e68cc8650390
hpark-tech <119441604+hpark-tech@users.noreply.github.com>
2024-03-18 00:57:04 +0900

    Update 붙임성 좋은 총총이.java

--- 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.java
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.java
@@ -1,24 +1,35 @@
+package baekjoon;
 
-
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        Set<String> set = new HashSet<>();
-        set.add("ChongChong");
-        StringTokenizer st;
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            String person1 = st.nextToken();
-            String person2 = st.nextToken();
-            if (set.contains(person1) || set.contains(person2)) {
-                set.add(person1);
-                set.add(person2);
-            }
-        }
-        System.out.println(set.size());
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		HashSet<String> hs = new HashSet<>();
+		int n = Integer.parseInt(br.readLine());
+		for (int i = 0; i < n; i++) {
+			StringTokenizer token = new StringTokenizer(br.readLine());
+			String one = token.nextToken();
+			String two = token.nextToken();
+			if (one.equals("ChongChong") || two.equals("ChongChong")) {
+				hs.add(one);
+				hs.add(two);
+			}
+			if (hs.contains(one)) {
+				hs.add(two);
+			}
+			if (hs.contains(two)) {
+				hs.add(one);
+			}
+		}
+		bw.write(hs.size() + "\n");
+		bw.flush();
+	}
 }
