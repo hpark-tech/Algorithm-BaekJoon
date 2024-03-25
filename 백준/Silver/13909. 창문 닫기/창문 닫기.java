40b6d045ffe54efc1545ec5827aa7fa13dbf473f
hpark-tech <119441604+hpark-tech@users.noreply.github.com>
2024-03-26 02:37:11 +0900

    Update 창문 닫기.java

--- 백준/Silver/13909. 창문 닫기/창문 닫기.java
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.java
@@ -1,17 +1,24 @@
-import java.io.*;
-import java.util.*;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-    public static void main(String args[]) throws IOException{
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-      int n = Integer.parseInt(br.readLine());
-
-      int cnt = 0;
-      for(int i=1; i*i<=n; i++){
-          cnt++;
-      }
-     
-      System.out.println(cnt);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine());
+		int result = 0;
+		for (long i = 1; i <= n; i++) {
+			if (i * i <= n) {
+				result++;
+			} else {
+				break;
+			}
+		}
+		bw.write(result + "\n");
+		bw.flush();
+	}
 }
