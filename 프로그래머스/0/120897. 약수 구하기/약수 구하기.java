0e14776d84f1be2ff1120023ccc71b69e91d3003
hpark-tech <119441604+hpark-tech@users.noreply.github.com>
2026-01-28 16:01:58 +0900

    [level 0] Title: 약수 구하기, Time: 0.18 ms, Memory: 87.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n) {
+      List<Integer> factors = new ArrayList<>();
+        for(int i=1; i<n+1; i++){
+if(n%i==0){
+    factors.add(i);
+}
+            }
+            int[] answer = new int[factors.size()];
+        for(int i = 0; i < factors.size(); i++) {
+            answer[i] = factors.get(i);
+        }
+        return answer;
+    }
+}
