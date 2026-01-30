708409564bcbcdb4fd297f6a17604bae0ffd53e9
hpark-tech <119441604+hpark-tech@users.noreply.github.com>
2026-01-30 11:39:16 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 44.47 ms, Memory: 117 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        for(int i:arr){if(stack.isEmpty()||stack.peek()!=i){stack.push(i);}}
+        int []answer = new int[stack.size()];
+        int cnt=0;
+        for(int i=0;i<answer.length;i++){answer[cnt++]=stack.pollLast();}
+        return answer;
+    }
+}
